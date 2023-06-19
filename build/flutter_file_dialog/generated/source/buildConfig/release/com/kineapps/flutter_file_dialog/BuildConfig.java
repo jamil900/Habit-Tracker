89e3f1064cf466e0ab5d2c22c5df5f6f8c5466cd
jamil900 <jamilulumu97@gmail.com>
2023-06-19 22:48:53 +0700

    first commit

---
+++ build/flutter_file_dialog/generated/source/buildConfig/release/com/kineapps/flutter_file_dialog/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.kineapps.flutter_file_dialog;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.kineapps.flutter_file_dialog";
+  public static final String BUILD_TYPE = "release";
+}
